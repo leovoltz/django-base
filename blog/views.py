6df96ca70eb82583e1f51fe73c75bc50049e44ee
Leonardo Pereira <ethical.leonardo@gmail.com>
2023-10-10 11:22:21 -0300

    adicionado reset no css, iniciado config  no html de forma dinâmica com Django HTML

--- blog/views.py
+++ blog/views.py
@@ -4,15 +4,27 @@ from django.shortcuts import render
 def blog(request):
     print('blog')
 
+    context = {
+        'text': 'Welcome to the blog',
+    }
+
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context
         )
 
 
 def exemplo(request):
     print('exemplo')
+
+    context = {
+        'text': 'Hello exemple',
+        'title': 'Example page - '
+    }
+
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context
         )
--- home/views.py
+++ home/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render
 def home(request):
     print('Home')
 
+    context = {
+        'text': 'Welcome Home'
+    }
+
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
         )
