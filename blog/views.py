0835ca0a30124d752916dfbf412c771328d05047
Leonardo Pereira <ethical.leonardo@gmail.com>
2023-10-10 15:29:52 -0300

    configurando urls dinâmicas, css basico para testar funcionamento e acessar links do 'blog'.

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,43 @@
+from typing import Any
+
 from django.shortcuts import render
+from blog.data import posts
+from django.http import HttpRequest
 
 
 def blog(request):
     print('blog')
 
     context = {
-        'text': 'Welcome to the blog',
+        'posts': posts
     }
 
     return render(
         request,
         'blog/index.html',
+        context
+        )
+
+
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = None
+
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+    if found_post is None:
+        raise Exception('Post não existe.')
+
+    context = {
+        'post': found_post,
+        'title': found_post['title'] + ' - ',
+    }
+
+    return render(
+        request,
+        'blog/post.html',
         context
         )
 
